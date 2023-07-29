708e57e58f5a41b5f83152de952d84853354e563
Ayushi Vashisth <107119119+AyushiVashisth@users.noreply.github.com>
2023-07-29 10:21:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -437,7 +437,7 @@ def generate_response():
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:4200"
+    response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PATCH, DELETE"
     return response
